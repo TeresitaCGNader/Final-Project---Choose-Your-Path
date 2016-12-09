21a80b916510c084f7ef1b6606ba75653e82d336
Teresita C. G. Nader <guzmannt@oregonstate.edu>
2016-12-08 21:50:03 -0800

    updated description

--- public/generate-dialog.js
+++ public/generate-dialog.js
@@ -1,15 +1,10 @@
-// Grab the todo template from Handlebars.
+// Grab the dialog template from Handlebars.
 var dialogTemplate = Handlebars.templates.dialog;
 
 /*
- * This function should create an HTML string representing a new todo note
- * given the information that could be in the note.
- *
- * Note that where, when, who, and details can be an empty string.  If this
- * is the case, the corresponding portion of the todo note should not be
- * included in the HTML string.
+ * This function should create an HTML string representing a new dialog page (which includes one dialog and 3 choices).
  */
-function generateTodoHTML(dialog, dialogText, choice, choice1Text, choice1Link, choice2Text, choice2Link, choice3Text, choice3Link) {
+function generateTodoHTML(dialogText, choice, choice1Text, choice1Link, choice2Text, choice2Link, choice3Text, choice3Link) {
   var data = {
     dialogText: dialogText,
     choice: {
