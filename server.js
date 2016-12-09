720f8c17d9df3b83f0e309f730e111f192da6047
DeadCatLady <cww.christa@gmail.com>
2016-12-08 17:03:13 -0800

    update on the comment button

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var bodyParser = require('body-parser');
 var mysql = require('mysql');
 
 var app = express();
-//var usersData = require('./users-data');
+var usersData = require('./dialogs-data');
 var port = process.env.PORT || 3000;
 
 /*
